0328cbe9ef154cbb736a05e7eaba2f09b58df6de
thomas burke <piecraft24@gmail.com>
2021-10-13 17:49:37 +0100

    Upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import { Header} from './componets/header'
+import { Footer} from './componets/footer'
+import { Content} from './componets/content'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav,Container}from 'react-bootstrap';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import { Create } from './componets/create';
+import { Read } from './componets/read';
+class App extends Component {
+  render (){
+    return(
+      <Router>
+    <div className="App">
+
+<Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/read">read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br/>
+  <Switch>
+<Route path='/' component={Content} exact/>//loads the content
+<Route path='/Create' component={Create} exact/>//loads the header
+<Route path='/read' component={Read} exact/>//loads the footer
+
+  </Switch>
+
+     
+    </div></Router>
+  );
+}
+}
+
+export default App;
---
+++ src/componets/create.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export class Create extends React.Component{//Returns below text when called
+render(){
+    return(
+        <div>
+            <h1>This is the Create Component</h1>
+        </div>
+    );
+}
+}
